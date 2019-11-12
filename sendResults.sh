a32c6b54011ab9296271f14a0480eda7facf581a
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-11-12 16:09:16 +0100

    Add the u flag to avoid spamming log

--- sendResults.sh
+++ sendResults.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -rvPh --delete /scratch/TestBuild/Coverage/html/ /afs/cern.ch/project/sixtrack/coverage/
-rsync -rvPh --delete /scratch/TestBuild/Results/       /afs/cern.ch/project/sixtrack/build_status/
-rsync -rvPh --delete /scratch/TestBuild/*.log          /afs/cern.ch/project/sixtrack/nightly_data/
-rsync -rvPh --delete /scratch/TestBuild/Timing/        /afs/cern.ch/project/sixtrack/nightly_data/tests/
-rsync -rvPh --delete /scratch/TestBuild/Coverage/*.tgz /afs/cern.ch/project/sixtrack/nightly_data/coverage/
+rsync -ruvPh --delete /scratch/TestBuild/Coverage/html/ /afs/cern.ch/project/sixtrack/coverage/
+rsync -ruvPh --delete /scratch/TestBuild/Results/       /afs/cern.ch/project/sixtrack/build_status/
+rsync -ruvPh --delete /scratch/TestBuild/*.log          /afs/cern.ch/project/sixtrack/nightly_data/
+rsync -ruvPh --delete /scratch/TestBuild/Timing/        /afs/cern.ch/project/sixtrack/nightly_data/tests/
+rsync -ruvPh --delete /scratch/TestBuild/Coverage/*.tgz /afs/cern.ch/project/sixtrack/nightly_data/coverage/
