ee6504c467918ef77ce1be4237c7bf39cc4f225c
Veronica K. B. Olsen <1619840+vkbo@users.noreply.github.com>
2019-11-12 16:05:00 +0100

    Added more data to be synced for Riccardo

--- sendResults.sh
+++ sendResults.sh
@@ -2,3 +2,6 @@
 
 rsync -rvPh --delete /scratch/TestBuild/Coverage/html/ /afs/cern.ch/project/sixtrack/coverage/
 rsync -rvPh --delete /scratch/TestBuild/Results/       /afs/cern.ch/project/sixtrack/build_status/
+rsync -rvPh --delete /scratch/TestBuild/*.log          /afs/cern.ch/project/sixtrack/nightly_data/
+rsync -rvPh --delete /scratch/TestBuild/Timing/        /afs/cern.ch/project/sixtrack/nightly_data/tests/
+rsync -rvPh --delete /scratch/TestBuild/Coverage/*.tgz /afs/cern.ch/project/sixtrack/nightly_data/coverage/
